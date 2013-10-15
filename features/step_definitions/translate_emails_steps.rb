ae0ce7b1885c847de6391e119c0f9bbcf0dd6c98
john irving <whimful@gmail.com>
2013-10-16 12:22:43 +1300

    fix failing spec, refactor

--- features/step_definitions/translate_emails_steps.rb
+++ features/step_definitions/translate_emails_steps.rb
@@ -90,7 +90,7 @@ end
 Then(/^"(.*?)" should receive the proposal closing soon email in Spanish$/) do |arg1|
   user = User.find_by_email("#{arg1}@example.org")
   email = UserMailer.motion_closing_soon(user, @motion)
-  email.body.encoded.should include("Grupo")
+  email.body.encoded.should include(I18n.t(:"email.proposal_closing_soon.closes_on", locale: "es"))
 end
 
 Given(/^"(.*?)" has closed their proposal$/) do |arg1|
